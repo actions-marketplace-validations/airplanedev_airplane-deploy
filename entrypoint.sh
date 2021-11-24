cb82f6b8681081a7927af6ee1b62013d40290835
Lee Weisberger <lweisberger5@gmail.com>
2021-11-24 15:25:23 -0500

    Pass in git metadata (#2)
    * Pass in git metadata
    
    * Pass in commit message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
-airplane --host $1 deploy $2
+branch=${5#"refs/heads/"}
+airplane --host $1 --repository $3 --gitUser $4 --branch $branch --commitHash $6 --commitMessage $7  deploy $2
