230179c3df0aa3afc33c147073ffe6c3c7b265bf
leeweisberger <lweisberger5@gmail.com>
2021-11-24 18:53:40 -0500

    only pass through repository and user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh -l
 
-branch=${5#"refs/heads/"}
-airplane --host $1 --repository $3 --gitUser $4 --branch $branch --commitHash $6 --commitMessage $7  deploy $2
+airplane --host $1 --repository $3 --gitUser $4 deploy $2
